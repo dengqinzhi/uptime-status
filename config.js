f263f29611aec5adff745056cb79e82b7d33d2a1
dengqinzhi <24427467@qq.com>
2023-07-10 08:08:43 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '邓先生的服务器监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1544101-a08bf56c565c63586fa5bb0e',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '博客',
+      url: 'https://qinzhi.cc'
+    },
+    {
+      text: '导航',
+      url: 'https://nav.qinzhi.cc'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/dengqinzhi'
+    },
+  ],
+};
